64ea6542d1061137ed5cd21955fe1ac994eb04ed
Omar Ojeda <omarsilva.ojeda@gmail.com>
2020-10-04 16:58:49 -0500

    Produccion Huayacan - cambiando footer

--- app/Views/includes/footer.php
+++ app/Views/includes/footer.php
@@ -4,14 +4,12 @@
             <div class="col-12 col-sm-5">
                 <!-- ftSocialNetworks -->
                 <ul class="list-unstyled ftSocialNetworks d-flex justify-content-center justify-content-sm-end mb-2 mb-sm-0">
-                    <li><a href="javascript:void(0);" class="fab fa-facebook-f"></a></li>
-                    <li><a href="javascript:void(0);" class="fab fa-twitter"></a></li>
-                    <li><a href="javascript:void(0);" class="fab fa-instagram"></a></li>
-                    <li><a href="javascript:void(0);" class="fab fa-vimeo-v"></a></li>
+                    <li><a target="_blank" href="https://www.facebook.com/parrillashuayacan" class="fab fa-facebook-f"></a></li>
+                    <li><a target="_blank" href="https://www.instagram.com/parrillashuayacan/" class="fab fa-instagram"></a></li>
                 </ul>
             </div>
             <div class="col-12 col-sm-7 text-center text-sm-left">
-                <span class="ftTxt d-block"><?= date('Y') ?> © <a href="javascript:void(0);"><b>Huayacan.</b></a> Todos los derechos reservados.</span>
+                <span class="ftTxt d-block"><?= date('Y') ?> © <a href="<?= base_url() ?>"><b>Huayacan.</b></a> Todos los derechos reservados.</span>
             </div>
         </div>
     </div>
